1243624e9504edc2ebd87b4f5241db4b00cb87a7
Xiaoyicarlo <mwxu@outlook.com>
2025-03-29 01:30:15 +0800

    scRNA

--- scRNA/test.r
+++ scRNA/test.r
@@ -2,3 +2,6 @@ library()
 library(Seurat)
 library(ggplot2)
 library(dplyr)
+
+a <- readRDS("data/SeuratObj.rds")
+a <- NormalizeData(a, normalization.method = "LogNormalize", scale.factor = 10000)  
